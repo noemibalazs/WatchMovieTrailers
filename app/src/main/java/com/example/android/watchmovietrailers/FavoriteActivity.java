de671bb1c3cee92e4e5f84d723a8057646866b2e
Noemi Balazs <balazsnoe@gmail.com>
2018-05-17 11:06:15 +0300

    Third

---
+++ app/src/main/java/com/example/android/watchmovietrailers/FavoriteActivity.java
@@ -0,0 +1,54 @@
+package com.example.android.watchmovietrailers;
+
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.GridView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.android.watchmovietrailers.data.MovieContract;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FavoriteActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private GridView mGridView;
+    private FavoriteAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorite);
+
+        mGridView = findViewById(R.id.grid);
+        mAdapter = new FavoriteAdapter(this, null);
+        mGridView.setAdapter(mAdapter);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        String [] projection = new String[]{
+                MovieContract.MovieEntry.MOVIE_IMAGE};
+        return new CursorLoader(FavoriteActivity.this, MovieContract.MovieEntry.CONTENT_URI, projection, null, null, null );
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        mAdapter.swapCursor(cursor);
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mAdapter.swapCursor(null);
+
+    }
+}
+
---
+++ app/src/main/java/com/example/android/watchmovietrailers/FavoriteAdapter.java
@@ -0,0 +1,33 @@
+package com.example.android.watchmovietrailers;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.ImageView;
+
+import com.example.android.watchmovietrailers.data.MovieContract;
+import com.squareup.picasso.Picasso;
+
+public class FavoriteAdapter extends CursorAdapter {
+
+    public FavoriteAdapter(Context context, Cursor cursor) {
+        super(context, cursor, 0);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return LayoutInflater.from(context).inflate(R.layout.grid_view_item, parent, false);
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+        int index = cursor.getColumnIndexOrThrow(MovieContract.MovieEntry.MOVIE_IMAGE);
+        String image = cursor.getString(index);
+        ImageView picture = view.findViewById(R.id.iv_image);
+        Picasso.with(context).load(image).into(picture);
+    }
+}
